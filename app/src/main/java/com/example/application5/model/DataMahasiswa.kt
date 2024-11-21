21072d73e26f141200dbbfe79633b938a4b1a507
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2024-11-21 18:25:34 +0700

    Menambahkan variabel nim, email, dan nohp di dataclass DataMahasiswa

--- app/src/main/java/com/example/application5/model/DataMahasiswa.kt
+++ app/src/main/java/com/example/application5/model/DataMahasiswa.kt
@@ -4,4 +4,7 @@ data class DataMahasiswa(
     val nama: String = "",
     val gender: String = "",
     val alamat: String = "",
-)
+    val nim: String = "",
+    val email: String = "",
+    val nohp: String = "",
+    )
